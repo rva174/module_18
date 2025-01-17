c910a8001d1f2c57ae17c080ca7ed9c94d5b6caa
rva174 <rva174@yandex.ru>
2025-01-17 09:05:38 +0500

    init

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from task2.views import func_template, ClassTemplate
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', func_template),
+    path('func/', func_template),
     path('class/', ClassTemplate.as_view()),
 ]
--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from django.views.generic import TemplateView
 
 def func_template(request):
